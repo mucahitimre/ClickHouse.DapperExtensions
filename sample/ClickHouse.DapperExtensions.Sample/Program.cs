04adf6ab9ab03b9a8f81b0f4465a789b185bcbcb
Mücahit İmre <mucahit.imre@proj-e.com>
2022-10-05 11:21:48 +0300

    fix

--- sample/ClickHouse.DapperExtensions.Sample/Program.cs
+++ sample/ClickHouse.DapperExtensions.Sample/Program.cs
@@ -1,13 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using ClickHouse.Ado;
+﻿using ClickHouse.Ado;
 using Dapper;
+using System;
+using System.Collections.Generic;
 
 namespace ClickHouse.DapperExtensions.Sample
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             var conn = new ClickHouseConnection("Compress=True;CheckCompressedHash=False;Compressor=lz4;Host=192.168.10.110;Port=9000;Database=geekbuying;User=default;Password=;");
             conn.Open();
@@ -15,12 +15,12 @@ namespace ClickHouse.DapperExtensions.Sample
 
             try
             {
-                var user=new TestUser(){ResisterDate = DateTime.Now,ResisterTime = DateTime.Now,Age = 18,Name = "张三2"};
-                var user2=new TestUser(){ResisterDate = DateTime.Now,ResisterTime = DateTime.Now,Age = 18,Name = "张三"};
-//                conn.Insert(user);
+                var user = new TestUser() { ResisterDate = DateTime.Now, ResisterTime = DateTime.Now, Age = 18, Name = "张三2" };
+                var user2 = new TestUser() { ResisterDate = DateTime.Now, ResisterTime = DateTime.Now, Age = 18, Name = "张三" };
+                //conn.Insert(user);
                 Console.WriteLine("Insert single success.");
 
-                conn.InsertBulk(new List<TestUser>(){ user, user2 });
+                conn.InsertBulk(new List<TestUser>() { user, user2 });
 
                 var users = conn.GetAll<TestUser>();
                 Console.WriteLine("Insert multi success.");
@@ -32,7 +32,7 @@ namespace ClickHouse.DapperExtensions.Sample
             conn.Close();
         }
 
-        static void CreateTable(ClickHouseConnection conn)
+        private static void CreateTable(ClickHouseConnection conn)
         {
             conn.Execute(
                 "CREATE TABLE IF NOT EXISTS TestUser (ResisterDate Date, ResisterTime DateTime, Name String, Age UInt16) ENGINE=MergeTree(ResisterDate,(ResisterTime,Name,Age), 8192)");
